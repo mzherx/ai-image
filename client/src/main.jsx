2f37c458dfb22f2b82e729d0a7c2be6b7c907531
MH Code <mazheriftikhar1@gmail.com>
2025-03-17 08:38:48 +0500

    17-March

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Browser } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import AppContextProvider from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <Browser>
+  <AppContextProvider>
     <App />
+  </AppContextProvider>
   </Browser>,
 )
