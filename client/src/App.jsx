93d1fa2c0ecc3215bbdd60a1d84b04741716bf49
MH Code <mazheriftikhar1@gmail.com>
2025-03-17 08:38:45 +0500

    17-March

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/result" element={<Result />} />
-        <Route path="/buy-credit" element={<BuyCredit />} />
+        <Route path="/buy" element={<BuyCredit />} />
       </Routes>
     </div>
   );
